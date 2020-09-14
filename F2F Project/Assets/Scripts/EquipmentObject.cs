4f092f1a2c34fab31ccb8a22ccefd566b5721a3c
Connor (Home) <connorcoxr@gmail.com>
2020-09-13 22:18:16 -0600

    Object system in place
    Objects can be created as a specific type of object. These are scriptable objects, which means they can be stored in inventory.

---
+++ F2F Project/Assets/Scripts/EquipmentObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+[CreateAssetMenu(fileName = "New Equipment Object", menuName = "Inventory System/Items/Equipment")]
+
+public class EquipmentObject : ItemObject
+{
+    public float atkBonus;
+    public float defBonus;
+
+    public void awake()
+    {
+        type = ItemType.Equipment;
+    }
+    
+}
